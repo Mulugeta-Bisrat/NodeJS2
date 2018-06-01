0469fe7e17d6af09a8cc23556b960827a9036f5b
Mulugeta-Bisrat <34776292+Mulugeta-Bisrat@users.noreply.github.com>
2018-05-31 17:58:30 -0700

    zip has been created

---
+++ zip.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var zlib = require('zlib');
+var gzib = zlib.createGzip();
+var readable = fs.createReadStream(__dirname + '/source.txt');
+var compressed = fs.createWriteStream(__dirname + '/destination.txt.gz');
+readable.pipe(gzib).pipe(compressed);
+
+var gUnzip = zlib.createGunzip();
+var read = fs.createReadStream(__dirname + '/destination.txt.gz');
+var uncomp = fs.createWriteStream(__dirname + '/unzipped.txt');
+
+read.pipe(gUnzip).pipe(uncomp);
+console.log(__dirname);
